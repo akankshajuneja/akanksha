fda155af6fb03f01281566cb58c4cec35977a129
Akanksha Juneja <aakankshajuneja@gmail.com>
2016-11-09 05:56:55 -0600

    change made to hellotoday

--- hellotoday.js
+++ hellotoday.js
@@ -2,11 +2,15 @@ var host = (process.env.VCAP_APP_HOST || 'localhost');
 var port = (process.env.VCAP_APP_PORT || 8080);
 var http = require('http');
 var server = http.createServer( function(request, response) {
-var body = 'Hello WebSphere!';
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var date = new Date();
+response.write(date.getDay());
+response.write("The day of the week is " + date.getDay() + ".");
 response.writeHead( 200, {
 'Content-Length': body.length,
 'Content-Type': 'text/plain'
 });
 response.write(body);
+response.end();
 });
 server.listen(port);
